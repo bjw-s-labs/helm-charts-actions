56bb3d1509b2009bfe71900f93e221c0a2cc0092
Bernd Schorgers <me@bjw-s.dev>
2025-02-19 09:37:07 +0100

    feat!: Archive a number of actions

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,8 @@
-// See: https://rollupjs.org/introduction/
-
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import json from '@rollup/plugin-json'
 
 export default [
-  {
-    input: 'src/collect-charts-action.ts',
-    output: {
-      esModule: true,
-      file: 'dist/collect-charts-action.js',
-      format: 'es',
-      sourcemap: false
-    },
-    plugins: [typescript(), nodeResolve(), commonjs()]
-  },
   {
     input: 'src/dereference-json-schema.ts',
     output: {
@@ -36,34 +23,4 @@ export default [
     },
     plugins: [typescript(), nodeResolve(), commonjs()]
   },
-  {
-    input: 'src/schemas.ts',
-    output: {
-      esModule: true,
-      file: 'dist/schemas.js',
-      format: 'es',
-      sourcemap: true
-    },
-    plugins: [typescript(), nodeResolve(), commonjs(), json()]
-  },
-  {
-    input: 'src/verify-chart-changelog-action.ts',
-    output: {
-      esModule: true,
-      file: 'dist/verify-chart-changelog-action.js',
-      format: 'es',
-      sourcemap: true
-    },
-    plugins: [typescript(), nodeResolve(), commonjs(), json()]
-  },
-  {
-    input: 'src/verify-chart-version-action.ts',
-    output: {
-      esModule: true,
-      file: 'dist/verify-chart-version-action.js',
-      format: 'es',
-      sourcemap: true
-    },
-    plugins: [typescript(), nodeResolve(), commonjs()]
-  }
 ]
